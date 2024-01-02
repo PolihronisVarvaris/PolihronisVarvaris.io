4d15f4a906af384a0b4dc1824f15199726a2398e
Polihronis Varvaris <polihronisv@gmail.com>
2024-01-02 20:08:43 +0200

    try2

--- js/scriptform.js
+++ js/scriptform.js
@@ -7,7 +7,7 @@ const form = document.getElementById("myForm");
         statusTxt.style.display = "block";
 
         let xhr = new XMLHttpRequest();
-        xhr.open("POST", "process.php", true);
+        xhr.open("POST", "message.php", true);
         xhr.onload = () => {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 let response = xhr.responseText;
