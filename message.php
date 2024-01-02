85ea0383dcb9cb59fbef7eaeadc665e325692f86
Polihronis Varvaris <75493778+PolihronisVarvaris@users.noreply.github.com>
2024-01-02 18:34:57 +0200

    Add files via upload

---
+++ message.php
@@ -0,0 +1,21 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    if(!empty($email) && !empty($message)){
+        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $receiver = "polihronisv@gmail.com";
+            $sumbject = "From: $name <$email>";
+            $body = "Name: $name\nEmail: $email\n\nMessage: $message";
+            $sender = "From: $email";
+            if(mail($receiver, $sumbject, $body, $sender)){
+                echo "Your message has been sent";
+            }
+        }else{
+            echo "Enter a valid email address!";
+        }
+    }else{
+        echo "Email and Name field is required";
+    }
+?>
