9c59b4f12535c131fe6ce85272880145f57eb667
Polihronis Varvaris <polihronisv@gmail.com>
2024-01-05 23:38:50 +0200

    commit css contact and bugs fix

--- js/scriptform.js
+++ js/scriptform.js
@@ -1,30 +1,120 @@
-//Contact Form in PHP
-const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
-form.onsubmit = (e)=>{
-  e.preventDefault();
-  statusTxt.style.color = "#0D6EFD";
-  statusTxt.style.display = "block";
-  statusTxt.innerText = "Sending your message...";
-  form.classList.add("disabled");
-
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "message.php", true);
-  xhr.onload = ()=>{
-    if(xhr.readyState == 4 && xhr.status == 200){
-      let response = xhr.response;
-      if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
-        statusTxt.style.color = "red";
-      }else{
-        form.reset();
-        setTimeout(()=>{
-          statusTxt.style.display = "none";
-        }, 3000);
+// Toggle Menu
+$(window).load(function() {
+    $(".btn-nav").on("click tap", function() {
+      $(".nav-content").toggleClass("showNav hideNav").removeClass("hidden");
+      $(this).toggleClass("animated");
+    });
+  });
+  
+  
+  // Filtered Portfolio
+  
+  var shuffleme = (function( $ ) {
+    'use strict';
+    var $grid = $('#grid'), //locate what we want to sort 
+        $filterOptions = $('.portfolio-sorting li'),  //locate the filter categories
+        $sizer = $grid.find('.shuffle_sizer'),    //sizer stores the size of the items
+  
+    init = function() {
+  
+      // None of these need to be executed synchronously
+      setTimeout(function() {
+        listen();
+        setupFilters();
+      }, 100);
+  
+      // instantiate the plugin
+      $grid.shuffle({
+        itemSelector: '[class*="col-"]',
+        sizer: $sizer    
+      });
+    },
+  
+        
+  
+  // Set up button clicks
+    setupFilters = function() {
+      var $btns = $filterOptions.children();
+      $btns.on('click', function(e) {
+        e.preventDefault();
+        var $this = $(this),
+            isActive = $this.hasClass( 'active' ),
+            group = isActive ? 'all' : $this.data('group');
+  
+        // Hide current label, show current label in title
+        if ( !isActive ) {
+          $('.portfolio-sorting li a').removeClass('active');
+        }
+  
+        $this.toggleClass('active');
+  
+        // Filter elements
+        $grid.shuffle( 'shuffle', group );
+      });
+  
+      $btns = null;
+    },
+  
+  
+  
+    listen = function() {
+      var debouncedLayout = $.throttle( 300, function() {
+        $grid.shuffle('update');
+      });
+  
+      // Get all images inside shuffle
+      $grid.find('img').each(function() {
+        var proxyImage;
+  
+        // Image already loaded
+        if ( this.complete && this.naturalWidth !== undefined ) {
+          return;
+        }
+  
+        // If none of the checks above matched, simulate loading on detached element.
+        proxyImage = new Image();
+        $( proxyImage ).on('load', function() {
+          $(this).off('load');
+          debouncedLayout();
+        });
+  
+        proxyImage.src = this.src;
+      });
+  
+      setTimeout(function() {
+        debouncedLayout();
+      }, 500);
+    };      
+  
+    return {
+      init: init
+    };
+  }( jQuery ));
+  
+  $(document).ready(function()
+  {
+    shuffleme.init(); //filter portfolio
+  });
+  
+  document.addEventListener('DOMContentLoaded', function () {
+    const photos = document.querySelectorAll('#photo-container img');
+    let currentIndex = 0;
+  
+    function showNextPhoto() {
+      if (currentIndex < photos.length - 1) {
+        photos[currentIndex].style.animation = 'none'; // Disable animation for current photo
+        currentIndex++;
+        photos[currentIndex].style.animation = 'fadeIn 1s forwards'; // Apply animation to next photo
       }
-      statusTxt.innerText = response;
-      form.classList.remove("disabled");
     }
-  }
-  let formData = new FormData(form);
-  xhr.send(formData);
-}
+  
+    // Trigger the animation when the page loads
+    showNextPhoto();
+  
+    // Set a timeout to keep only the last image after the animation finishes
+    setTimeout(() => {
+      photos[currentIndex].style.animation = 'none'; // Disable animation for the last photo
+    }, photos.length * 1000); // Adjust the timeout based on the duration of your animation
+  });
+
+  
